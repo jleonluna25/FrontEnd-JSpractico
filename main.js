7a5fe5a17bce3664119250223b147943f123f356
Jesus Leon <jleonluna25@gmail.com>
2022-08-16 17:09:43 -0500

    #3. correccion efectos de display de los menus

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const navbarMenu = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const hamburguerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+mobileMenu.classList.remove("activo");
+
 navbarMenu.addEventListener("click", toggleDesktopMenu);
+hamburguerMenu.addEventListener("click", toggleMobileMenu);
+
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("activo");
 }
